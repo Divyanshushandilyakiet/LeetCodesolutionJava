05fd04b3a63cdfb89301e8f0c8688c8b724a8873
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2022-10-06 16:20:47 +0530

    Create  Revstring.java

---
+++ Revstring.java
@@ -0,0 +1,15 @@
+class Revstring {
+public void reverseString(char[] str) {
+    
+    int len = str.length;
+    
+    for (int i = 0; i < len / 2; i++) {
+
+        char temp = str[i];
+        str[i] = str[len-i-1];
+        str[len-i-1] = temp;
+
+    }
+    
+}
+}
